962da4830e6b3fa6258abe5245e999be1be5873f
JOSEPH <JHOPSMC@GMAIL.COM>
2023-04-20 21:51:15 -0500

    bare bones

---
+++ prj/src/main.cpp
@@ -0,0 +1,34 @@
+// KThisIsAConstant
+// this_is_a_variable
+// ThisIsAFunction
+// ThisIsAClass
+// ThisIsAnEnum
+// this_is_a_member_variable_
+// kThisIsAnEnumMember
+
+
+
+
+// C headers
+
+// C++ headers
+
+// std headers
+#include <iostream>
+
+// library headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+
+// project headers
+#include "shader.h"
+#include "stb_image.h"
+
+int main() {
+
+
+  return 0;
+}
