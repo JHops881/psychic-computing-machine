3bf1aab99f9ef8302f2dc80c30e73618d18640f7
JOSEPH <JHOPSMC@GMAIL.COM>
2023-04-22 16:40:42 -0500

    fullscreen baby

--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -35,8 +35,8 @@ void FrameBufferSizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window, plr::Player& player);
 bool glfwSetWindowCenter(GLFWwindow* window);
 
-const unsigned int kScreenWidth = 800;
-const unsigned int kScreenHeight = 600;
+const unsigned int kScreenWidth = 1920;
+const unsigned int kScreenHeight = 1080;
 const char* kWindowTitle = "Game";
 
 int main() {
@@ -47,7 +47,7 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  GLFWwindow* window = glfwCreateWindow(kScreenWidth, kScreenHeight, kWindowTitle, NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(kScreenWidth, kScreenHeight, kWindowTitle, glfwGetPrimaryMonitor(), NULL);
   if (window == NULL) {
     std::cout << "GLFW FAILED TO INIT" << std::endl; // still making it
     glfwTerminate();
@@ -60,7 +60,6 @@ int main() {
     return -1;
   }
  
-  glfwSetWindowCenter(window);
   glViewport(0, 0, kScreenWidth, kScreenHeight); 
   glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback); // Done!
 
@@ -94,7 +93,7 @@ int main() {
 
   // defining the projection matrix
   glm::mat4 projection_mat = glm::mat4(1.0f);
-  projection_mat = glm::perspective(glm::radians(80.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+  projection_mat = glm::perspective(glm::radians(80.0f), (float)kScreenWidth /(float)kScreenHeight, 0.1f, 100.0f);
 
   // setting the uniform matrixes to be corresponding to the ones above
   shader.SetMat4fv("projection", projection_mat);
@@ -150,7 +149,7 @@ int main() {
 
     // defining the view matrix per frame because it changes with movement
     glm::mat4 view_mat = glm::mat4(1.0f);
-    view_mat = glm::translate(view_mat, (glm::vec3(0.0f, 0.0f, -6.0f) -= player.GetPos()));
+    view_mat = glm::translate(view_mat, (glm::vec3(0.0f, 0.0f, -7.0f) -= player.GetPos()));
     shader.SetMat4fv("view", view_mat);
 
     // player
@@ -200,78 +199,4 @@ void ProcessInput(GLFWwindow* window, plr::Player& player) {
     player.MoveAmount(glm::vec3(0.1f, 0.0f, 0.0f));
   }
 
-}
-
-// https://github.com/glfw/glfw/issues/310 | sagaceilo commented on Jun 21, 2014
-bool glfwSetWindowCenter(GLFWwindow* window) {
-  if (!window)
-    return false;
-
-  int sx = 0, sy = 0;
-  int px = 0, py = 0;
-  int mx = 0, my = 0;
-  int monitor_count = 0;
-  int best_area = 0;
-  int final_x = 0, final_y = 0;
-
-  glfwGetWindowSize(window, &sx, &sy);
-  glfwGetWindowPos(window, &px, &py);
-
-  // Iterate throug all monitors
-  GLFWmonitor** m = glfwGetMonitors(&monitor_count);
-  if (!m)
-    return false;
-
-  for (int j = 0; j < monitor_count; ++j)
-  {
-
-    glfwGetMonitorPos(m[j], &mx, &my);
-    const GLFWvidmode* mode = glfwGetVideoMode(m[j]);
-    if (!mode)
-      continue;
-
-    // Get intersection of two rectangles - screen and window
-    int minX = std::max(mx, px);
-    int minY = std::max(my, py);
-
-    int maxX = std::min(mx + mode->width, px + sx);
-    int maxY = std::min(my + mode->height, py + sy);
-
-    // Calculate area of the intersection
-    int area = std::max(maxX - minX, 0) * std::max(maxY - minY, 0);
-
-    // If its bigger than actual (window covers more space on this monitor)
-    if (area > best_area)
-    {
-      // Calculate proper position in this monitor
-      final_x = mx + (mode->width - sx) / 2;
-      final_y = my + (mode->height - sy) / 2;
-
-      best_area = area;
-    }
-
-  }
-
-  // We found something
-  if (best_area)
-    glfwSetWindowPos(window, final_x, final_y);
-
-  // Something is wrong - current window has NOT any intersection with any monitors. Move it to the default one.
-  else
-  {
-    GLFWmonitor* primary = glfwGetPrimaryMonitor();
-    if (primary)
-    {
-      const GLFWvidmode* desktop = glfwGetVideoMode(primary);
-
-      if (desktop)
-        glfwSetWindowPos(window, (desktop->width - sx) / 2, (desktop->height - sy) / 2);
-      else
-        return false;
-    }
-    else
-      return false;
-  }
-
-  return true;
 }
